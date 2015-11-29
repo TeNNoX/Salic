65162c71ffeef17e9c71f606d2cbdbf8024d1bc9
TeNNoX <manuel.tennox@gmail.com>
2015-11-29 18:14:22 +0100

    Print exception in save.php

--- save.php
+++ save.php
@@ -2,29 +2,33 @@
 
 namespace salic;
 
-if (!array_key_exists('pagekey', $_POST)) {
-    http_response_code(400);
-    die("Error: missing pagekey in POST data");
+try {
+    if (!array_key_exists('pagekey', $_POST)) {
+        http_response_code(400);
+        die("Error: missing pagekey in POST data");
+    }
+    if (!array_key_exists('regions', $_POST)) {
+        http_response_code(400);
+        die("Error: missing regions in POST data");
+    }
+    $pagekey = $_POST['pagekey'];
+    $regions = $_POST['regions'];
+
+    require_once('salic/Salic.php');
+
+    $salic = new Salic();
+    $salic->loadPages();
+
+    if (!array_key_exists($pagekey, $salic->pages)) {
+        //TODO: error handling
+        http_response_code(400);
+        die("Error: Unknown pagekey '$pagekey'");
+    }
+
+    $salic->savePage($pagekey, $regions);
+    echo "success";
+
+} catch (\Exception $e) {
+    die("Exception while trying to save:<br>\n$e");
 }
-if (!array_key_exists('regions', $_POST)) {
-    http_response_code(400);
-    die("Error: missing regions in POST data");
-}
-$pagekey = $_POST['pagekey'];
-$regions = $_POST['regions'];
-
-require_once('salic/Salic.php');
-
-$salic = new Salic();
-$salic->loadPages();
-
-if (!array_key_exists($pagekey, $salic->pages)) {
-    //TODO: error handling
-    http_response_code(400);
-    die("Error: Unknown pagekey '$pagekey'");
-}
-
-$salic->savePage($pagekey, $regions);
-echo "success";
-
 ?>
