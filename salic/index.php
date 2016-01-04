5200434097cef90fc1f89373876dc1e70f8282e6
TeNNoX <manuel.tennox@gmail.com>
2016-01-04 02:07:24 +0100

    Always redirect to language prefixed pages
    / -> /en/

--- salic/index.php
+++ salic/index.php
@@ -10,16 +10,15 @@ $lang_settings = Settings::getLangSettings();
 if (array_key_exists('lang', $_GET)) {
     $lang = $_GET['lang'];
     if (!array_key_exists($lang, $lang_settings['available'])) {
-        echo "Invalid Language: $lang"; //TODO:
-    }
-} else {
-    $lang = Utils::getDefaultLanguageFromHeader();
-    if ($lang != $lang_settings['default']) { // if a better language is available, redirect to it
-        echo "Redirect: <a href='/$lang/$page'>/$lang/$page</a>";
-        http_response_code(303); //TODO: how to redirect properly for localisation
-        header("Location:/$lang/$page");
+        echo "Invalid Language: $lang"; //TODO: ignore invalid language?
         exit;
     }
+} else {
+    $lang = Utils::getDefaultLanguageFromHeader();  // language is not given, redirect to the best one
+    echo "Redirect: <a href='/$lang/$page'>/$lang/$page</a>";
+    http_response_code(303); //TODO: how to redirect properly for localisation
+    header("Location:/$lang/$page");
+    exit;
 }
 
 $salic = new Salic($lang);
