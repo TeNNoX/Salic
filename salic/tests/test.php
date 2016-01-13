5ddfd3cf5c74845ec03411c2bc53d03bc313bc25
TeNNoX <manuel.tennox@gmail.com>
2016-01-13 01:25:44 +0100

    Add lang_detection test

---
+++ salic/tests/test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace salic;
+
+chdir('../../');
+
+require_once 'salic/Utils.php';
+require_once 'salic/Settings.php';
+
+?>
---
+++ salic/tests/test_lang_detection.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace salic;
+
+require_once 'test.php';
+
+function test_language_detection()
+{
+    echo "<b>Your header:</b> ".$_SERVER["HTTP_ACCEPT_LANGUAGE"];
+    $lang_settings = Settings::getLangSettings();
+
+    $default = $lang_settings['default'];
+    $available = array_keys($lang_settings['available']);
+
+    require_once 'salic/LanguageDetection.php';
+
+    $values = array(
+        'de' => "de",
+        'en' => "en",
+        'de-DE' => "de",
+        'DE-dE' => "de",
+        'en-FUCKME' => "en",
+        'en-GB' => "en",
+        'en-US' => "en",
+        'de,en-US;q=0.7,en;q=0.3' => "de",
+        'de;q=0.1,en-US;q=0.7,en;q=0.3' => "en",
+        'de-DE,en-US;q=0.7,en;q=0.3' => "de",
+        'de-DE;q=0.1,en-US;q=0.7,en;q=0.3' => "en",
+        'pt,en-US;q=0.7,en;q=0.3' => "pt",
+        'pt;q=0.1,en-US;q=0.7,en;q=0.3' => "en",
+        'pt-PT,en-US;q=0.7,en;q=0.3' => "pt",
+        'pt-PT;q=0.1,en-US;q=0.7,en;q=0.3' => "en",
+    );
+
+    echo "<table border='1'><tr> <th>Header</th> <th>Parsed</th> <th>Result</th> <th>Expected</th> </tr>";
+    foreach ($values as $val => $expected) {
+        echo "<tr><td><i>$val</i></td> <td>";
+        $result = parseDefaultLanguage($val, $available, $default, true);
+        $bg = $result == $expected ? "green" : "red";
+        echo "</td> <td bgcolor='$bg'><b>$result</b></td>";
+        echo "<td>$expected</td> </tr>";
+    }
+    echo "</table>";
+}
+
+test_language_detection();
+
+?>
