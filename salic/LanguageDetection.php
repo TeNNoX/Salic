58d188052f648914d5693a07453cbe3dc35655ba
TeNNoX <manuel.tennox@gmail.com>
2016-01-13 01:14:05 +0100

    Ignore language subtags for now

--- salic/LanguageDetection.php
+++ salic/LanguageDetection.php
@@ -16,29 +16,38 @@ function getDefaultLanguage($available, $default = 'en')
         return parseDefaultLanguage(NULL, $available, $default);
 }
 
-function parseDefaultLanguage($http_accept, $available, $deflang = 'en')
+function parseDefaultLanguage($http_accept, $available, $deflang = 'en', $debugPrint = false)
 {
-    echo "http: $http_accept, avail: " . var_export($available, true) . ", default: $deflang\n<br>";
     if (isset($http_accept) && strlen($http_accept) > 1 && sizeof($available) > 0) {
-        if (sizeof($available) == 1)
+        if (sizeof($available) == 1) // no point in detection, if there's only one available...
             return strtolower($available[0]);
 
-        # Split possible languages into array
+        // Split possible languages into array
         $x = explode(",", $http_accept);
         $lang = array();
         foreach ($x as $val) {
-            #check for q-value and create associative array. No q-value means 1 by rule
+            // check for q-value and create associative array. No q-value means 1 by rule
             if (preg_match("/(.*);q=([0-1]{0,1}.\d{0,4})/i", $val, $matches))
                 $lang[$matches[1]] = (float)$matches[2];
             else
                 $lang[$val] = 1.0;
         }
 
-        #return default language (highest q-value)
+        // return default language (highest q-value)
         $qval = 0.0;
+        if ($debugPrint)
+            echo "<pre>" . var_export($lang, true) . "</pre>";
+
         foreach ($lang as $key => $value) {
-            if (!in_array(strtolower($key), $available)) // skip if not available
-                continue;
+            // ignore special identifiers (eg 'en-US'), TODO: language subtag matching
+            if (preg_match("/^(\w+)-(\w+)/", $key, $matches)) {
+                if ($debugPrint)
+                    echo "reducing " . $key . " to " . $matches[1] . "...<br>";
+                $key = $matches[1];
+            }
+
+            if (!in_array(strtolower($key), $available))
+                continue; // skip if not available
 
             if ($value > $qval) {
                 $qval = (float)$value;
@@ -46,7 +55,6 @@ function parseDefaultLanguage($http_accept, $available, $deflang = 'en')
             }
         }
     }
-    echo "RESULT: ".$deflang."\n<br>";
     return strtolower($deflang);
 }
 
